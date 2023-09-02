5cb6a516a8ab802428b473e65cf3db0d6f853ac9
Ebenezer Agyekum <agyekumebenezer2001@gmail.com>
2023-09-02 23:05:45 +0000

    started the main comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import RootLayout from "./layout";
+import { LandingPage } from "@/components/main/LandingPage";
 
 export default function Home() {
   return (
     <>
-      <main className={styles.main}></main>
+      <main>
+        <LandingPage />
+      </main>
     </>
   );
 }
