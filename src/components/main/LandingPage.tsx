1124564343ecb82b980532f009c0542a1f52efcc
Ebenezer Agyekum <agyekumebenezer2001@gmail.com>
2023-09-03 23:44:02 +0000

    finished working text comp

--- src/components/main/LandingPage.tsx
+++ src/components/main/LandingPage.tsx
@@ -1,17 +1,30 @@
 "use client";
-import { Paper, Text } from "@mantine/core";
+import { Paper, Text, createStyles, rem } from "@mantine/core";
 import React from "react";
 import "./main.css";
 import { myFont } from "@/app/layout";
 export const LandingPage = () => {
+  const { classes } = useStyles();
+
   return (
     <Paper className="main">
       <Text
-        sx={{ fontSize: "200px" }}
+        sx={{ fontSize: "300px", textAlign: "center", lineHeight: "300px" }}
         className={`${myFont.className} textGrad`}
       >
-        My Story
+        About
+        <br /> Me
       </Text>
     </Paper>
   );
 };
+
+const useStyles = createStyles(() => ({
+  inner: {
+    display: "flex",
+    justifyContent: "space-between",
+    width: "80rem",
+    alignItems: "center",
+    borderRadius: "20px",
+  },
+}));
