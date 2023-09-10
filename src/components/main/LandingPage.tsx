198901b0a63a3f215ed77e788032c52ad7fb4bf0
Ebenezer Agyekum <agyekumebenezer2001@gmail.com>
2023-09-10 23:53:54 +0100

    added the hero section

--- src/components/main/LandingPage.tsx
+++ src/components/main/LandingPage.tsx
@@ -3,6 +3,9 @@ import { Paper, Text, createStyles, rem } from "@mantine/core";
 import React from "react";
 import "./main.css";
 import { myFont } from "@/app/layout";
+import hero from "../../assets/d82c112c-f406-4023-82d5-f3079ea4d0a9_storyHero.avif";
+import Image from "next/image";
+
 export const LandingPage = () => {
   const { classes } = useStyles();
 
@@ -14,7 +17,9 @@ export const LandingPage = () => {
       >
         About
         <br /> Me
+        <Image src={hero} alt="jo" className={classes.mainImage} />
       </Text>
+      <Text sx={{ textAlign: "center" }}>This is something I love doing</Text>
     </Paper>
   );
 };
@@ -26,5 +31,9 @@ const useStyles = createStyles(() => ({
     width: "80rem",
     alignItems: "center",
     borderRadius: "20px",
+  },
+  mainImage: {
+    position: "relative",
+    top: "-20rem",
   },
 }));
